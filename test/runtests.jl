2dfc649e4a9334bfd3490d7090959a69c2ea714e
Michel Geradin <mgeradin@gmail.com>
2026-05-28 08:48:38 +0200

    runtests introduced 28/05/2026

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
+
+using  LinearAlgebra
 using ExpMap
 using Test
 
 @testset "ExpMap.jl" begin
-    # Write your tests here.
+    include("tests_ExpMap.jl")
+    res = tests_ExpMap()
+    @test res < 1e-12
 end
